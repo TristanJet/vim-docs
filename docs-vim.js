6fbfcc4e5663a24d62c64827238f048e99d0d378
TristanJet <tristanjetlay1@gmail.com>
2024-09-22 16:10:07 +0300

    c and word text object

--- docs-vim.js
+++ docs-vim.js
@@ -59,6 +59,7 @@ vim.switchToInsertMode = function() {
 
 // Called in normal mode.
 vim.normal_keydown = function(e) {
+    console.log(`NORMAL: ${e.key} - pressed`);
     if (e.key.match(/F\d+/)) {
         // Pass through any function keys.
         return true;
@@ -78,18 +79,17 @@ vim.normal_keydown = function(e) {
     }
 
     if (e.key == "d") {
-        console.log("NORMAL: d - pressed")
         if (vim.currentSequence === "d") {
             vim.currentSequence = "";
             docs.pressKey(40, true, false);
             docs.pressKey(38, true, false); // to guarantee that cursor is at the start
             docs.pressKey(40, true, true); // Shift + down goes to end of the line
-            docs.cdoc.execCommand("copy"); // Copy the line, cut doesn't work for some reason            
+            docs.cdoc.execCommand("copy"); // Copy the line, cut doesn't work for some reason
             docs.setColor("red");
             setTimeout(() => {
                 docs.setColor("black")
                 docs.pressKey(46); // Delete key to remove the selected line
-            }, 100)
+            }, 50)
             return true;
         } else {
             vim.currentSequence = "d";
@@ -97,8 +97,27 @@ vim.normal_keydown = function(e) {
         }
     }
 
+    if (e.key == "c") {
+        if (vim.currentSequence === "c") {
+            vim.currentSequence = "";
+            docs.pressKey(40, true, false);
+            docs.pressKey(38, true, false); // to guarantee that cursor is at the start
+            docs.pressKey(40, true, true); // Shift + down goes to end of the line
+            docs.setColor("red");
+            setTimeout(() => {
+                docs.setColor("black")
+                docs.pressKey(46); // Delete key to remove the selected line
+            }, 50)
+            vim.switchToInsertMode()
+            return true;
+        } else {
+            vim.currentSequence = "c";
+            return true;
+        }
+    }
+
+    /* Bring cursor back to original position???*/
     if (e.key == "y") {
-        console.log("NORMAL: y - pressed")
         if (vim.currentSequence === "y") {
             vim.currentSequence = "";
             docs.pressKey(40, true, false);
@@ -108,8 +127,8 @@ vim.normal_keydown = function(e) {
             docs.setColor("red");
             setTimeout(() => {
                 docs.setColor("black")
-                docs.pressKey(38, true, true); // Up arrow to return to original position
-            }, 100)
+                docs.pressKey(38, true, true); // Up arrow to return to start of line
+            }, 50)
             return true;
         } else {
             vim.currentSequence = "y";
@@ -117,8 +136,45 @@ vim.normal_keydown = function(e) {
         }
     }
 
+    //39 right, 37 left
+    if (e.key == "w") {
+        if (vim.currentSequence === "y") {
+            vim.currentSequence = "";
+            docs.pressKey(39, true, true)
+            docs.cdoc.execCommand("copy")
+            docs.setColor("red");
+            setTimeout(() => {
+                docs.setColor("black")
+                docs.pressKey(37, true, true); // Up arrow to return to start of line
+            }, 50)
+            return true
+        } else if (vim.currentSequence === "d") {
+            vim.currentSequence = "";
+            docs.pressKey(39, true, true); // Shift + down goes to end of the line
+            docs.cdoc.execCommand("copy"); // Copy the line, cut doesn't work for some reason
+            docs.setColor("red");
+            setTimeout(() => {
+                docs.setColor("black")
+                docs.pressKey(46); // Delete key to remove the selected line
+            }, 50)
+            return true
+        } else if (vim.currentSequence === "c") {
+            vim.currentSequence = "";
+            docs.pressKey(39, true, true); // Shift + down goes to end of the line
+            docs.setColor("red");
+            setTimeout(() => {
+                docs.setColor("black")
+                docs.pressKey(46); // Delete key to remove the selected line
+            }, 50)
+            vim.switchToInsertMode();
+            return true
+
+        } else {
+            vim.currentSequence = "";
+        }
+    }
+
     if (e.key == "g") {
-        console.log("NORMAL: g - pressed")
         if (vim.currentSequence === "g") {
             vim.currentSequence = "";
             docs.pressKey(36, true, false); // Ctrl + Home (go to start of document)
@@ -130,14 +186,12 @@ vim.normal_keydown = function(e) {
     }
 
     if (e.key == "G") {
-        console.log("NORMAL: G - pressed")
         vim.currentSequence = "";
         docs.pressKey(35, true, false); // Ctrl + End (go to end of document)
         return true;
     }
 
     if (e.key == "p") {
-        console.log("NORMAL: p pressed!")
         docs.clip.readText().then(
             (cltxt) => docs.pasteText(cltxt)
         ).catch(
@@ -169,6 +223,7 @@ vim.normal_keydown = function(e) {
 
 // Called in visual mode.
 vim.visual_keydown = function(e) {
+    console.log(`VISUAL: ${e.key} - pressed`);
     if (e.key.match(/F\d+/)) {
         // Pass through any function keys.
         return true;
@@ -182,20 +237,30 @@ vim.visual_keydown = function(e) {
         return false;
     }
 
-    if (e.key == "y" || e.key == "d") {
+    if (e.key == "y" || e.key == "d" || e.key == "c") {
         e.preventDefault();
         e.stopPropagation();
         if (e.key == "y") {
-            console.log("VISUAL: y - pressed!");
             docs.cdoc.execCommand("copy");
             vim.switchToNormalMode();
             docs.setColor("red");
-            setTimeout(() => docs.setColor("black"), 100)
+            setTimeout(() => docs.setColor("black"), 50)
+            return true
+        } else if (e.key == "d") {
+            vim.switchToNormalMode();
+            docs.setColor("red");
+            setTimeout(() => {
+                docs.setColor("black")
+                docs.cdoc.execCommand("cut");
+            }, 50)
             return true
         } else {
-            console.log("VISUAL: d - pressed");
-            docs.cdoc.execCommand("cut");
-            vim.switchToNormalMode();
+            docs.setColor("red");
+            setTimeout(() => {
+                docs.setColor("black")
+                docs.pressKey(46); // Delete key to remove the selected line
+                vim.switchToInsertMode();
+            }, 50)
             return true
         }
     }
@@ -208,7 +273,6 @@ vim.visual_keydown = function(e) {
     }
 
     if (e.key == "g") {
-        console.log("VISUAL: g - pressed")
         if (vim.currentSequence === "g") {
             vim.currentSequence = "";
             docs.pressKey(36, true, true); // Ctrl + Shift + Home (select to start of document)
@@ -220,7 +284,6 @@ vim.visual_keydown = function(e) {
     }
 
     if (e.key == "G") {
-        console.log("VISUAL: G - pressed")
         vim.currentSequence = "";
         docs.pressKey(35, true, true); // Ctrl + Shift + End (select to end of document)
         return true;
